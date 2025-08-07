7472b56958151a6d8a1ef796644a0e93c2e43ecd
Superstar <39806623+kailee55n@users.noreply.github.com>
2025-08-06 23:16:29 -0500

    Update cli_printchain.go
    Co-Authored-By: Nicholas Frayne <17525375+nicholas555-dev@users.noreply.github.com>

--- cli_printchain.go
+++ cli_printchain.go
@@ -14,7 +14,11 @@ func (cli *CLI) printChain(nodeID string) {
 	for {
 		block := bci.Next()
 
-		fmt.Printf("============ Block %x ============\n", block.Hash)
+		fmt.Printf("============ Block %x ============\n", block.Hash) // Print block hash
+		fmt.Printf("Timestamp: %s\n", block.Timestamp)
+		fmt.Printf("Transactions: %d\n", len(block.Transactions))
+		fmt.Printf("Nonce: %d\n", block.Nonce)
+		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Printf("Height: %d\n", block.Height)
 		fmt.Printf("Prev. block: %x\n", block.PrevBlockHash)
 		pow := NewProofOfWork(block)
